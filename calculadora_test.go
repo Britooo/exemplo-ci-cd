066f4035ce2a594a5b99a5834514c22304ddebe2
Diego Brito <diego.brito@sptech.school>
2025-05-21 21:27:53 -0300

    exemplo falha

--- calculadora_test.go
+++ calculadora_test.go
@@ -2,20 +2,25 @@ package main
 
 import "testing"
 
+type cenario struct {
+	a        int
+	b        int
+	esperado int
+}
+
 func TestSomar(t *testing.T) {
 
-    cenarios := []struct {
-        a, b, esperado int
-    }{
-        {2, 3, 5},
-        {0, 0, 0},
-        {-1, 1, 0},
-    }
+	cenarios := []cenario{
+		{2, 3, 6},
+		{0, 0, 0},
+		{-1, 1, 0},
+	}
 
-    for _, c := range cenarios {
-        resultado := Somar(c.a, c.b)
-        if resultado != c.esperado {
-            t.Errorf("resultado '%d', esperado '%d'", resultado, c.esperado)
-        }
-    }
+	for _, c := range cenarios {
+		resultado := Somar(c.a, c.b)
+		if resultado != c.esperado {
+			t.Errorf("resultado '%d', esperado '%d'",
+				resultado, c.esperado)
+		}
+	}
 }
